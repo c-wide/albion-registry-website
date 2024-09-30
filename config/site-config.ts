d580dc8d94d285bedaebda7ebca61045512bda56
c-wide <53365073+c-wide@users.noreply.github.com>
2024-09-30 12:59:40 -0500

    fix updated api base url

--- config/site-config.ts
+++ config/site-config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
 	websiteName: "Albion Registry",
 	albionServerRegions: ["Americas", "Asia", "Europe"],
-	albionRegistryBaseApiUrl: "https://albion-registry-website.vercel.app",
+	albionRegistryBaseApiUrl: "https://albion-registry-api.fly.dev",
 	discordInviteUrl: "https://discord.gg/x86PwqWtDv",
 	githubRepoUrl:
 		"https://github.com/c-wide?tab=repositories&q=albion&type=&language=&sort=",
