0b47bf43b07669d171449f8cd47ff2062c33bdd8
c-wide <53365073+c-wide@users.noreply.github.com>
2024-10-23 11:10:15 -0500

    revert albion registry base api url change

--- config/site-config.ts
+++ config/site-config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
 	websiteName: "Albion Registry",
 	albionServerRegions: ["Americas", "Asia", "Europe"],
-	albionRegistryBaseApiUrl: "https://albion-registry-api.fly.dev",
+	albionRegistryBaseApiUrl: "https://registry-api.albion.tools",
 	discordInviteUrl: "https://discord.gg/x86PwqWtDv",
 	githubRepoUrl:
 		"https://github.com/c-wide?tab=repositories&q=albion&type=&language=&sort=",
